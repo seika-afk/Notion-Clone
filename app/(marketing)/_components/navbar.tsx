c24acdc9275ad49f5b6aaaead4abc83c40313f89
Seika <artistnoob6@gmail.com>
2025-07-04 01:42:21 +0530

    3june

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,29 +4,42 @@ import useScrollTop from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
 
+export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  const scrolled = useScrollTop();
 
+  return (
+    <div
+      className={cn(
+        "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <Logo />
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <p>Loading...</p>}
 
-export const Navbar=()=>{
-const scrolled=useScrollTop();
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
 
+            <SignInButton mode="modal">
+              <Button size="sm">Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
 
-	return (
+        <ModeToggle />
+      </div>
+    </div>
+  );
+};
 
-
-<div className={cn("z-50 bg-background  dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6"
-,scrolled && " border-b shadow-sm "  )}>
-<Logo/>
-
-<div className="md:ml-auto md:justify-end justify-between  w-full flex items-center gap-x-2">
- 
-<ModeToggle/>
-</div>
-
-
-</div>
-
-)
-
-
-}
